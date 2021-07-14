a6de11f9669eef70304e87494921347c56bb98fa
hexingyu <hxy@cx5y.cn>
2021-07-14 11:24:09 +0800

    提交页面

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
+const path = require('path')
 const Koa = require('koa')
+const koaStatic = require('koa-static')
 const app = new Koa()
+
+app.use(koaStatic(path.join(__dirname, 'html')))
+
+// x-response-time
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.set('X-Response-Time', `${ms}ms`)
+})
+
+// logger
+app.use(async (ctx, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`)
+})
 
 app.use(async ctx => {
   ctx.body = 'Hello World'
 })
 
-app.listen(process.env.PORT || 3000)
+// 要部署到Heroku上端口号必须得是process.env.PORT
+app.listen(process.env.PORT || 3000, () => {
+  console.log('启动成功')
+})
